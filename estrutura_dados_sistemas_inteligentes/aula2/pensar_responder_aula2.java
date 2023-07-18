a983b00427632c157cec82f1b453da27569060d7
Marcio Cunha <marcioabcd11@gmail.com>
2023-07-18 11:33:53 -0300

    estrutura de dados aula 2

---
+++ estrutura_dados_sistemas_inteligentes/aula2/pensar_responder_aula2.java
@@ -0,0 +1,20 @@
+package estrutura_dados_sistemas_inteligentes.aula2;
+
+import javax.swing.JOptionPane;
+
+public class pensar_responder_aula2 {
+    public static void main (String args []){
+        int i;
+        double notas[], media, soma = 0;
+        notas = new double[5];
+
+        for (i = 0; i < 5; i++){
+            notas[i] = Double.parseDouble(JOptionPane.showInputDialog("Digite a nota"));
+            soma = soma + notas[i];
+        }
+
+        media = soma / 5;
+        System.out.println("A média das notas é: " + media);
+        System.exit(0);
+    }
+}
